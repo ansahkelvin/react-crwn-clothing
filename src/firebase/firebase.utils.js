a84800ff7badcd899493f7d42ee2dd8de120d596
Ansah Kelvin Nana Osei <46853302+ansahkelvin@users.noreply.github.com>
2022-03-01 16:14:10 +0000

    Moving to Mac

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,40 +4,40 @@ import "firebase/compat/firestore";
 import "firebase/compat/auth";
 
 const config = {
-    apiKey: "AIzaSyBY7LHf8kGb7jUxBvgkQsYhXskzYGfgqVM",
-    authDomain: "react-crwn-clothing-f770a.firebaseapp.com",
-    projectId: "react-crwn-clothing-f770a",
-    storageBucket: "react-crwn-clothing-f770a.appspot.com",
-    messagingSenderId: "120007430815",
-    appId: "1:120007430815:web:7052cce72c201e64ef08aa",
-    measurementId: "G-1C193D2S20",
+  apiKey: "AIzaSyBY7LHf8kGb7jUxBvgkQsYhXskzYGfgqVM",
+  authDomain: "react-crwn-clothing-f770a.firebaseapp.com",
+  projectId: "react-crwn-clothing-f770a",
+  storageBucket: "react-crwn-clothing-f770a.appspot.com",
+  messagingSenderId: "120007430815",
+  appId: "1:120007430815:web:7052cce72c201e64ef08aa",
+  measurementId: "G-1C193D2S20",
 };
 
 const app = firebase.initializeApp(config);
 export const analytics = getAnalytics(app);
 
-export const createUserProfileDocument = async(userAuth, additionalData) => {
-    if (!userAuth) return;
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
 
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
-    const snapshot = await userRef.get();
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
 
-    if (!snapshot.exists) {
-        const { displayName, email } = userAuth;
-        const createdAt = new Date();
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
 
-        try {
-            await userRef.set({
-                displayName,
-                email,
-                createdAt,
-                ...additionalData,
-            });
-        } catch (error) {
-            console.log(error.message);
-        }
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log(error.message);
     }
-    return userRef;
+  }
+  return userRef;
 };
 
 export const auth = firebase.auth();
